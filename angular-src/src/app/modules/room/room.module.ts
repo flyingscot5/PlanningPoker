4be7f24ca7c513449de93d010d48e6eb4f217956
Callum <25401022+flyingscot5@users.noreply.github.com>
2022-03-04 16:06:11 +0000

    adding new user card

--- angular-src/src/app/modules/room/room.module.ts
+++ angular-src/src/app/modules/room/room.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RoomPageComponent} from './room-page/room-page.component';
 import { CardComponent } from './components/card/card.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
 
 
 @NgModule({
   declarations: [
     RoomPageComponent,
-    CardComponent
+    CardComponent,
+    UserCardComponent
   ],
   exports: [
     RoomPageComponent
