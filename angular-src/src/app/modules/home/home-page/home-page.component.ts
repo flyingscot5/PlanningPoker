8f3d476a44bd0556b99cc4322d0a7c6f5ee0fd9c
Callum <25401022+flyingscot5@users.noreply.github.com>
2022-03-04 12:27:25 +0000

    added button to create a new room

--- angular-src/src/app/modules/home/home-page/home-page.component.ts
+++ angular-src/src/app/modules/home/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+  private _router: Router;
 
-  constructor() { }
+  constructor(router: Router) {
+    this._router = router;
+  }
 
   ngOnInit(): void {
+  }
+
+  createRoom() {
+    const num = Math.random().toString(36).substr(2, 9);
+    this._router.navigateByUrl('/room/' + num);
   }
 
 }
